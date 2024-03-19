4d311377ccd549ce5ea0e9489fea2a64bf4c151e
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2024-03-19 14:13:21 +0100

    La til autentisering

---
+++ src/middleware/urls.ts
@@ -0,0 +1,9 @@
+import { getEnvironment } from "@src/urls";
+
+const REDIRECT_URI = {
+  development: "https://www.intern.dev.nav.no/tms-utkast-frontend",
+  production: "https://www.intern.nav.no/tms-utkast-frontend",
+};
+
+export const redirectUri = REDIRECT_URI[getEnvironment()];
+export const loginUrl = `/minside/oauth2/login?redirect=${redirectUri}`;
