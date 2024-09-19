882ad27da71caabeba441af6a8dee9f2f565c798
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2024-09-19 17:40:29 +0200

    Server-side render empty utkast

--- src/utils/client/urls.ts
+++ src/utils/client/urls.ts
@@ -13,18 +13,10 @@ export const getEnvironment = () => {
 
 type EnvUrl = { development: string; production: string; local: string };
 
-const MIN_SIDE_PROXY_URL = {
-  local: "http://localhost:3000",
-  development: "https://www.ansatt.dev.nav.no/tms-min-side-proxy",
-  production: "https://www.nav.no/tms-min-side-proxy",
-};
-
 const BASE_URL: EnvUrl = {
   local: "http://localhost:3000",
   development: "https://www.ansatt.dev.nav.no/minside/utkast/",
   production: "https://www.nav.no/minside/utkast/",
 };
 
-export const minSideProxyUrl = MIN_SIDE_PROXY_URL[getEnvironment()];
-// export const utkastApiUrl = `${minSideProxyUrl}/utkast/v2/utkast`;
 export const baseUrl = BASE_URL[getEnvironment()];
