d956a4fbec2711a0b822027c4225b8c63eb142d3
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2024-10-31 09:47:45 +0100

    Lagt inn logging av skjemakode og skjemanavn

--- src/utils/client/amplitude.tsx
+++ src/utils/client/amplitude.tsx
@@ -2,23 +2,14 @@ import { type MetricValues } from "@components/utkast/UtkastTypes.ts";
 
 import { init, track } from "@amplitude/analytics-browser";
 
-export const logAmplitudeEvent = (
-  skjemaurl: string,
-  metrics: MetricValues | null | undefined
-) => {
-  if (metrics) {
-    track("skjema åpnet", { skjemaurl, ...metrics });
-  } else {
-    track("skjema åpnet", { skjemaurl });
-  }
-};
-
 const initUtkastClickTracking = () => {
   const utkastWrapper = document.getElementById("utkastWrapper");
+
   utkastWrapper?.addEventListener("click", (event  ) => {
     const target = (event?.target as HTMLElement).closest("a");
     const targetHref = target!.href;
-    logAmplitudeEvent(targetHref, null);
+    const { utkastSkjemanavn, utkastSkjemakode } = target?.dataset || {};
+    track("skjema åpnet", { targetHref, utkastSkjemanavn, utkastSkjemakode });
   });
 };
 
