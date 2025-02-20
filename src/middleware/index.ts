d7d0a2bb835a7cff5af47350d74af0dbd2938659
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2025-02-20 12:39:41 +0100

    Bruk Pino for logging

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -3,10 +3,12 @@ import { defineMiddleware } from "astro/middleware";
 import { loginUrl } from "./urls";
 import { isInternal } from "./utils";
 import { isLocal } from "@src/utils/server/urls.ts";
+import pino from "pino-http";
 
 export const onRequest = defineMiddleware(async (context, next) => {
   const bearerToken: string | null | undefined = context.request.headers.get("authorization");
   const params = encodeURIComponent(context.url.search);
+  const logger = pino().logger;
 
   if (isLocal) {
     return next();
@@ -17,7 +19,7 @@ export const onRequest = defineMiddleware(async (context, next) => {
   }
 
   if (!bearerToken) {
-    console.info("Could not find any bearer token on the request. Redirecting to login.");
+    logger.info("Could not find any bearer token on the request. Redirecting to login.");
     return context.redirect(`${loginUrl}${params}`);
   }
 
@@ -25,7 +27,7 @@ export const onRequest = defineMiddleware(async (context, next) => {
 
   if (validationResult !== "valid") {
     const error = new Error(`Invalid JWT token found (cause: ${validationResult.errorType} ${validationResult.message}, redirecting to login.`);
-    console.error(error);
+    logger.error(error);
     return context.redirect(`${loginUrl}${params}`);
   }
 
