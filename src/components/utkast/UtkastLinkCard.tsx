0f9e6a14df9522a115b041aa8ba061de3d6731ac
Nima <nima.hakimi@nav.no>
2026-02-13 15:00:06 +0100

    Use linkCard component instead of manually creating a lincard

---
+++ src/components/utkast/UtkastLinkCard.tsx
@@ -0,0 +1,30 @@
+import { LinkCard, Tag } from "@navikt/ds-react";
+import type { Language } from "@src/language/language";
+import { text } from "@src/language/text";
+import dayjs from "dayjs";
+import type { UtkastElement } from "./UtkastTypes";
+
+interface UtkastLinkCardProps {
+  utkast: UtkastElement;
+  language: Language;
+}
+
+export default function UtkastLinkCard({
+  utkast,
+  language,
+}: UtkastLinkCardProps) {
+  const dateFormatter = (date: string) => dayjs(date).format("DD.MM.YYYY");
+
+  return (
+    <LinkCard data-analytics-id="utkast-åpnet">
+      <LinkCard.Title>
+        <LinkCard.Anchor href={utkast.link}>{utkast.tittel}</LinkCard.Anchor>
+      </LinkCard.Title>
+      <LinkCard.Footer>
+        <Tag variant="info" size="small">
+          {`${text.opprettet[language]}: ${dateFormatter(utkast.opprettet)}`}
+        </Tag>
+      </LinkCard.Footer>
+    </LinkCard>
+  );
+}
