d7652dfce3aefd879f910d04044a257fbac83f61
Nima <nima.hakimi@nav.no>
2026-02-23 13:22:59 +0100

    Upgrade to aksel v8  and adjust tag styling

--- src/components/utkast/UtkastLinkCard.tsx
+++ src/components/utkast/UtkastLinkCard.tsx
@@ -2,7 +2,6 @@ import { LinkCard, Tag } from "@navikt/ds-react";
 import type { Language } from "@src/language/language";
 import { text } from "@src/language/text";
 import dayjs from "dayjs";
-import styles from "./Utkast.module.css";
 import type { UtkastElement } from "./UtkastTypes";
 
 interface UtkastLinkCardProps {
@@ -17,12 +16,12 @@ export default function UtkastLinkCard({
   const dateFormatter = (date: string) => dayjs(date).format("DD.MM.YYYY");
 
   return (
-    <LinkCard data-analytics-id="utkast-åpnet" className={styles.linkCard}>
+    <LinkCard data-analytics-id="utkast-åpnet">
       <LinkCard.Title>
         <LinkCard.Anchor href={utkast.link}>{utkast.tittel}</LinkCard.Anchor>
       </LinkCard.Title>
       <LinkCard.Footer>
-        <Tag variant="info-moderate" size="medium">
+        <Tag variant="moderate" data-color="info" size="medium">
           {`${text.opprettet[language]} ${dateFormatter(utkast.opprettet)}`}
         </Tag>
       </LinkCard.Footer>
