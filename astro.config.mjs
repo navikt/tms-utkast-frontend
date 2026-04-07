ed2c922202c25f1f6e1364b957d9daed4dfd311c
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2026-04-07 17:13:29 +0200

    Fjern importmap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import node from "@astrojs/node";
 import react from "@astrojs/react";
 import { defineConfig, envField } from "astro/config";
-import { rollupImportMapPlugin } from "rollup-plugin-import-map";
-import importmap from "./importmap.json";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,23 +8,7 @@ export default defineConfig({
   build: {
     assetsPrefix: "https://cdn.nav.no/min-side/tms-utkast-frontend",
   },
-  integrations: [
-    react(),
-    {
-      name: "importmap",
-      hooks: {
-        "astro:build:setup": ({ vite, target }) => {
-          if (target === "client") {
-            vite.plugins.push({
-              ...rollupImportMapPlugin(importmap),
-              enforce: "pre",
-              apply: "build",
-            });
-          }
-        },
-      },
-    },
-  ],
+  integrations: [react()],
   i18n: {
     defaultLocale: "nb",
     locales: ["nb", "nn", "en"],
